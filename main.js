3f7917be76c7b0c22a4c35cf19066e81ab885154
notnome <118110408+notnomes@users.noreply.github.com>
2023-08-28 19:10:54 +0500

    v1.10

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-const log = async (string) => {
-    console.log(string)
-}
+const log = (s) => console.log(string) // prints the (s) object :)
+const err = (s) => console.log(`\x1b[41m%s\x1b[0m ${s}`, `   `) // this is prints the string with error text on start with backround
+const warn = (s) => console.log(`\x1b[43m%s\x1b[0m ${s}`, ` Warn `) // this is warn command with colors :)
+const succ = (s) => console.log(`\x1b[42m%s\x1b[0m ${s}`, ` Ok `) // this is prints string in object with backround color
 
-const logError = async (string) => {
-    console.log(`\x1b[41m%s\x1b[0m ${string}`, ` ERROR `)
-}
-const logWarn = async (string) => {
-    console.log(`\x1b[43m%s\x1b[0m ${string}`, ` Warn `)
-}
-const logOk = async (string) => {
-    console.log(`\x1b[42m%s\x1b[0m ${string}`, ` Ok `)
-}
-
-module.exports = {log, logError, logWarn, logOk};
+module.exports = {log, warn, err, succ} // exports all functions :)
