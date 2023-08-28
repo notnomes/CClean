c367f346ba0562140aa917a61000a9c4948ab1fa
notnome <118110408+notnomes@users.noreply.github.com>
2023-08-28 09:16:03 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-function log(string) {
+const log = async (string) => {
     console.log(string)
 }
 
-module.exports = [log]
+const logError = async (string) => {
+    console.log(`\x1b[41m%s\x1b[0m ${string}`, ` ERROR `)
+}
+const logWarn = async (string) => {
+    console.log(`\x1b[43m%s\x1b[0m ${string}`, ` Warn `)
+}
+const logOk = async (string) => {
+    console.log(`\x1b[42m%s\x1b[0m ${string}`, ` Ok `)
+}
+
+module.exports = {log, logError, logWarn, logOk};
