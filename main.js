9ab8bae6208e0184f84ca7e430f3db6ea5f6e34d
notnome <118110408+notnomes@users.noreply.github.com>
2023-08-28 19:29:21 +0500

    v1.20

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const log = (s) => console.log(string) // prints the (s) object :)
-const err = (s) => console.log(`\x1b[41m%s\x1b[0m ${s}`, `   `) // this is prints the string with error text on start with backround
-const warn = (s) => console.log(`\x1b[43m%s\x1b[0m ${s}`, ` Warn `) // this is warn command with colors :)
-const succ = (s) => console.log(`\x1b[42m%s\x1b[0m ${s}`, ` Ok `) // this is prints string in object with backround color
+const log = (s) => console.log(s) // prints the (s) object :)
+const err = (s) => console.log(`\x1b[41m%s\x1b[0m ${s}`, ` \x1b[1mERR \x1b[0m`) // this is prints the string with error text on start with backround
+const warn = (s) => console.log(`\x1b[43m%s\x1b[0m ${s}`, ` \x1b[1mWARN \x1b[0m`) // this is warn command with colors :)
+const succ = (s) => console.log(`\x1b[42m%s\x1b[0m ${s}`, ` \x1b[1mOK \x1b[0m`) // this is prints string in object with backround color
 
-module.exports = {log, warn, err, succ} // exports all functions :)
+module.exports = {log, warn, err,succ} // exports all functions :)
